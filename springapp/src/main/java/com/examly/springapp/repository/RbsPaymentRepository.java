65cd859193df121a3790d4ce4dddce706f344b6f
PRT01 <mrpratyush2000@gmail.com>
2023-07-19 13:17:10 +0000

    Payment controller ,model and repo added.

--- springapp/src/main/java/com/examly/springapp/repository/RbsPaymentRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/RbsPaymentRepository.java
@@ -1,12 +1,17 @@
 package com.examly.springapp.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.examly.springapp.model.RbsPayment;
-
+import java.util.List;
+import com.examly.springapp.model.RbsBooking;
 
 
 public interface RbsPaymentRepository extends JpaRepository<RbsPayment, Integer>{
 
-   // List<RbsPayment> findByBooking(RbsBooking booking);
+
+    List<RbsPayment> findByBooking(RbsBooking booking);
     
 }
+
+
