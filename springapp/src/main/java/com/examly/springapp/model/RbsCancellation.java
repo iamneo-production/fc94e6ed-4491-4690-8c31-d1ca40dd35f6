097d1cb12c7ce9e2286708d0387b73cac01d00a0
Anitha-Ani03 <anisankar311@gmail.com>
2023-07-20 06:21:41 +0000

    Cancellation Backend

--- springapp/src/main/java/com/examly/springapp/model/RbsCancellation.java
+++ springapp/src/main/java/com/examly/springapp/model/RbsCancellation.java
@@ -1,10 +1,93 @@
+
 package com.examly.springapp.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Column;
 
 
+import java.util.Date;
 
 
+import javax.persistence.Table;
+
+
+@Entity
+@Table
 public class RbsCancellation {
 
-   
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int cancellationId;
+
+
+    private Date dateCancelled;
+    
+    @Column(length = 255)
+    private String reason;
+    
+    @ManyToOne
+    @JoinColumn(name="booking_id")
+    private Booking booking;
+    
+    
+    public Booking getBooking() {
+        return booking;
+    }
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    } 
+
+
+    public RbsCancellation() {
+        super();
+        
+    }
+
+
+    public RbsCancellation(int cancellationId, Date dateCancelled, String reason) {
+        super();
+        this.cancellationId = cancellationId;
+        this.dateCancelled = dateCancelled;
+        this.reason = reason;
+    }
+
+
+    public int getCancellationId() {
+        return cancellationId;
+    }
+
+
+    public void setCancellationId(int cancellationId) {
+        this.cancellationId = cancellationId;
+    }
+
+
+    public Date getDateCancelled() {
+        return dateCancelled;
+    }
+
+
+    public void setDateCancelled(Date dateCancelled) {
+        this.dateCancelled = dateCancelled;
+    }
+
+
+    public String getReason() {
+        return reason;
+    }
+
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+    
     
 }
+
+
+
--- springapp/src/main/java/com/examly/springapp/repository/RbsCancellationRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/RbsCancellationRepository.java
@@ -1,12 +1,20 @@
 package com.examly.springapp.repository;
 
+
 import com.examly.springapp.model.RbsCancellation;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import com.examly.springapp.model.RbsBooking;
 
 
 public interface RbsCancellationRepository extends JpaRepository<RbsCancellation, Integer>{
 
-    //List<RbsCancellation> findByBooking(RbsBooking booking);
+
+    List<RbsCancellation> findByBooking(RbsBooking booking);
     
 }
+
+
+
