a6661db75f83f7bfd5baf5bce3859521d507fede
jyotimondal <jyotisvs2001@gmail.com>
2023-07-21 10:35:08 +0000

    About us and contact us pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import HotelBookingForm from './HotelBookingForm'
 import Control from './Control';
 import Login from './Login';
 import Home from './Home';
+import ContactUs from './ContactUs';
+import About from './About';
 
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
       <Route path='/landing' element={<Home/>}></Route>
       <Route path='/register' element={<Control />}></Route>
       <Route path='/login' element={<Login />}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/contact' element={<ContactUs />}></Route>
       </Routes>
     </BrowserRouter>
   );
